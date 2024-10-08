5ae720c849a42c9f7993169cc071b38e5c676c87
vidhi udasi <udasividhi18@gmail.com>
2024-10-08 21:57:17 +0530

    Add files via upload

---
+++ cookingwater.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class cookingwater{
+public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+     int N=sc.nextInt();   
+    for(int i=0;i<N;i++){
+        
+        int a=sc.nextInt(); 
+        //sc.next();
+        int b=sc.nextInt();
+        if(a!=b){
+            System.out.println("edward is right");
+        }
+        else{
+            System.out.println("gunilla has a point ");
+        }
+    }
+}}
