3e919ca33c3834b93578009f794a133a6f787f4f
vidhi udasi <udasividhi18@gmail.com>
2024-10-10 17:07:44 +0530

    Add files via upload

---
+++ shortcuttowhat.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class shortcuttowhat{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int n2=((n+5)*3);
+        int n3=n2-10;
+        System.out.println(n3);
+        
+}}
