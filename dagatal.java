62faba639d4cd0bef942209904c4d868de439b69
vidhi udasi <udasividhi18@gmail.com>
2024-10-13 23:07:03 +0530

    Update dagatal.java

--- dagatal.java
+++ dagatal.java
@@ -3,16 +3,44 @@ public class dagatal{
     public static void main(String[]args ){
       Scanner s = new Scanner(System.in);
       int m = s.nextInt();
-      
-          if(m==2){
-              System.out.println(28);
-          }
-          else if(m/2==0){
-             System.out.println(30); 
-              
-          }
-          else{
-              System.out.println(31);
+          switch(m){
+  case 1:
+    System.out.println(31);
+    break;
+  case 2:
+    System.out.println(28);
+    break;
+  case 3:
+    System.out.println(31);
+    break;
+  case 4:
+    System.out.println(30);
+    break;
+  case 5:
+    System.out.println(31);
+    break;
+  case 6:
+    System.out.println(30);
+    break;
+    case 7:
+    System.out.println(31);
+    break;
+    case 8 :
+    System.out.println(31);
+    break;
+    case 9 :
+    System.out.println(30);
+    break;
+    case 10:
+    System.out.println(31);
+    break;
+    case 11:
+    System.out.println(30);
+    break;
+    case 12:
+    System.out.println(31);
+    break;
           }
       }
     }
+
