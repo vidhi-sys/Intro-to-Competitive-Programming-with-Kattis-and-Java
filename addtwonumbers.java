777f73794783a228ab20460c642af958448f6651
vidhi udasi <udasividhi18@gmail.com>
2024-10-16 22:58:28 +0530

    Add files via upload

---
+++ addtwonumbers.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class addtwonumbers{
+    public static void main(String[]args){      
+    Scanner s = new Scanner(System.in);
+    int a = s.nextInt();
+    int b = s.nextInt();
+        System.out.println(a+b);
+    }}
